b794fee906144a52ed57347e358081f35667c232
eljc00 <elise.j.choi@gmail.com>
2019-03-10 21:25:07 -0700

    Add epicodus JS loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
+var total = 0;
+for (var currentNumber = 1; currentNumber <= 300; currentNumber += 1) {
+  total += currentNumber;
+}
 
 
-var numberz = [];
-    for (var index = 1; index <= numberz; index +=1) {
-    if (numberz % 3 === 0) {
-        $(result).text("Ping");
-    } else if (numberz % 5 === 0) {
-        $(result).text("Pong");
-    } else  if (numberz % 15 === 0) {
-        $(result).text("Ping Pong");
-    } else {
-        $(result).text("numberz");
-    }   
-    }
-  };
-  $(document).ready(function() {
-    $("form#pingpongform").submit(function(event) {
-      event.preventDefault();
-      var numberz = parseInt($("input#numberz").val());
-      var result = pingPongNumber(year);
-      $("#result").text(result);
-    });
-  });
+// var numberz = [];
+//     for (var index = 1; index <= numberz; index +=1) {
+//     if (numberz % 3 === 0) {
+//         $(result).text("Ping");
+//     } else if (numberz % 5 === 0) {
+//         $(result).text("Pong");
+//     } else  if (numberz % 15 === 0) {
+//         $(result).text("Ping Pong");
+//     } else {
+//         $(result).text("numberz");
+//     }   
+//     }
+//   };
+//   $(document).ready(function() {
+//     $("form#pingpongform").submit(function(event) {
+//       event.preventDefault();
+//       var numberz = parseInt($("input#numberz").val());
+//       var result = pingPongNumber(year);
+//       $("#result").text(result);
+//     });
+//   });
