9c36273656edcedcc4ee276b5690ef1995daca4e
Student <example@example.com>
2019-03-11 17:47:59 -0700

    Fiddle with submit function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
 var total = 0;
 for (var i = 1; i <= 300; i += 1) {
   total += i;
-}
-
-for (var i=1; i <= 300; i++){
     if (i % 15 == 0) {
-    document.write("FizzBuzz");
+    document.write("PingPong");
     } else if (i % 3 == 0) {
-    document.write("Fizz");
+    document.write("Ping");
     } else if (i % 5 == 0) {
-    document.write("Buzz");
+    document.write("Pong");
     } else
     document.write(i);
     document.write('<br>');
 }
 
 $(document).ready(function() {
-  // $("form#pingpongform").submit(function(event) {
+  $("#pingpongform").submit(function(event) {
+    $("result").text(result);
     event.preventDefault();
     // var numberz = parseInt($("input#numberz").val());
-    var result = pingPongNumber(year);
-    $("#result").text(result);
+    // var result = total();
+
   });
 });
 // var numberz = [];
